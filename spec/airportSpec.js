f41a238f47ec8cbcac9baccd181104834510f33d
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-06 15:43:19 +0000

    airport clear plane tests refactored

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -2,13 +2,20 @@
 
 describe('Airport', function(){
   var airport;
+  var plane;
 
   beforeEach(function(){
     airport = new Airport();
+    plane = jasmine.createSpy('plane',['land']);
   });
 
   it('has no planes by default', function(){
     expect(airport.hangar()).toEqual([]);
   });
-  
+
+  it('can clear planes for landing', function(){
+    airport.clearPlaneForLanding(plane);
+    expect(airport.hangar()).toEqual([plane]);
+  });
+
 });
